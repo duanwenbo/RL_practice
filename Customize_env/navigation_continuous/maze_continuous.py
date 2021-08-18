1eee724e6b3d4f9edcd55858f63ba630419f6d39
duanwenbo <bobbyduanwenbo@live.com>
2021-08-18 22:29:18 +0100

    continuous navigation environment with VPG

---
+++ Customize_env/navigation_continuous/maze_continuous.py
@@ -0,0 +1,56 @@
+from gym import Env
+import gym
+from gym.envs.registration import make
+from gym.spaces import Discrete, Box
+import numpy as np
+import random
+from scipy.interpolate import interp1d
+import math
+import torch
+import torch.nn.functional as F
+from torch import tensor
+
+
+
+class Maze(Env):
+    metadata = {"render.modes":["console"]}
+    def __init__(self) -> None:
+        super(Maze, self).__init__()
+        self.action_space = Box(low=np.array([-1]), high=np.array([1]))
+        self.observation_space = Box(low=np.array([0,0]), high=np.array([10,10]))
+        self.state = np.array([9.0,9.0])
+        self.episode_length = 100
+        # self.action_dict = {"0":(0,0.1),"1":(0.1,0.1),"2":(0.1,0),"3":(0.1,-0.1),"4":(0,-0.1),"5":(-0.1,-0.1),"6":(-0.1,0),"7":(-0.1,0.1),"8":(0.0,0.0)}
+        # self.goal = (random.uniform(0,10), random.uniform(0,10))
+        self.goal = (1.0, 1.0) # for model test
+        self.map = interp1d([-1,1], [0,360])
+
+    def step(self, action):
+        self.episode_length -= 1
+        action = self.map(np.tanh(action))
+        x_increment, y_increment = 0.3 * math.cos(math.radians(action)), 0.3 * math.sin(math.radians(action))
+        self.state = (self.state[0]+ x_increment, self.state[1]+y_increment)
+        distance = ((self.state[0] - self.goal[0])**2 + (self.state[1] - self.goal[1])**2)**0.5
+        amplify_factor= max([0.008, self.episode_length / 100]) # normalized amplified factor
+        reward = (-distance) / amplify_factor
+        if self.episode_length <=0:
+            done = True
+        elif distance <= 0.05:
+            done = True
+            reward += 500
+        else:
+            done = False
+        return np.array(self.state), reward, done, distance
+
+    def reset(self):
+        self.state =  np.array([9.0,9.0])
+        self.episode_length = 100
+        return self.state
+
+    def render(self):
+        pass
+        
+
+if __name__ == "__main__":
+    maze = Maze()
+    print(maze.step(60))
---
+++ Customize_env/navigation_continuous/vpg.py
@@ -0,0 +1,177 @@
+"""
+to verify the customize model
+"""
+
+from gym import envs
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical, Normal
+
+import gym
+import numpy as np
+import wandb
+from maze_continuous import Maze
+
+env = Maze()
+wandb.init(project="customize_env_vpg_continuous")
+
+"""
+changed advantage function
+"""
+
+# policy network. to find the optimal policy and choose action
+class Policy_net(nn.Module):
+    def __init__(self, input=env.observation_space.shape[0], hidden=64, output=2):
+        super(Policy_net, self).__init__()
+        self.fc1 = nn.Linear(input, hidden)
+        self.fc2 = nn.Linear(hidden, hidden)
+        self.fc3 = nn.Linear(hidden, output)
+
+    def forward(self, input):
+        x = F.relu(self.fc1(input))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)  # for probability
+        return x
+
+
+# state value estimate network, to find the optimal state value and choose the state value
+class V_net(nn.Module):
+    def __init__(self, input=env.observation_space.shape[0], hidden=64, output=1):
+        super(V_net, self).__init__()
+        self.fc1 = nn.Linear(input, hidden)
+        self.fc2 = nn.Linear(hidden, hidden)
+        self.fc3 = nn.Linear(hidden, output)
+
+    def forward(self, input):
+        x = F.relu(self.fc1(input))
+        x = F.relu((self.fc2(x)))
+        x = self.fc3(x)
+        return x
+
+
+# choosing action based on state, using the policy network
+def choose_action(state):
+    """
+    :param state: a 1x4 numpy array, which is returned from gym env
+    :return: action: init (1 or 0), used as input to this gym environment
+    """
+    state = torch.as_tensor(state, dtype=torch.float32)  # convert from numpy to tensor
+    # probabilities = Categorical(policy_net(state).detach())  # prediction don't need to back propagate
+    mu, sigma = policy_net(state)
+    distribution = Normal(mu, sigma)
+    action = distribution.sample()
+    log_prob = distribution.log_prob(action)
+    return action, log_prob
+
+
+def compute_advantage(rewards, state_values):
+    """
+    # advantages = reward_to_go - state_value
+    :param rewards: list, rewards of each episode
+    :param state_values: list, estimated by v net
+    :return: (tensor, tensor)
+    """
+    # rtgs
+    reward2gos = np.zeros_like(rewards)
+    for i in reversed(range(len(rewards))):
+        reward2gos[i] = rewards[i] + (reward2gos[i + 1] if i + 1 < len(rewards) else 0)  # [R0+R1+R2, R1+R2, R3..]
+
+    advantages = reward2gos - np.array(state_values)
+    return torch.as_tensor(advantages, dtype=torch.float32), torch.as_tensor(reward2gos, dtype=torch.float32)
+
+
+def comput_policy_loss(log_probs, rewards, state_values):
+    """
+    loss = - mean ( sum( log_prob * advantage ) )
+    """
+    advantages = compute_advantage(rewards, state_values)[0]
+    loss = 0
+    for log_prob, advantage in zip(log_probs, advantages):
+        step_loss = torch.mul(log_prob, advantage)
+        loss += step_loss
+    policy_loss = - (loss / len(advantages))
+    return policy_loss
+
+
+def verify():
+    # a separate func to test the training results of policy net
+    rewards = 0
+    state = env.reset()
+    while True:
+        action = choose_action(state)[0]  # only for verification
+        next_state, reward, done, distance = env.step(action)
+        state = next_state
+        rewards += reward
+        if done:
+            break
+    return rewards, distance
+
+
+# hyper parameters
+episode_num = 10000
+gamma = 0.98
+learning_rate = 0.0005
+
+# initialization
+policy_net = Policy_net()
+v_net = V_net()
+policy_optimizer = torch.optim.Adam(policy_net.parameters(), lr=learning_rate)
+v_optimizer = torch.optim.Adam(v_net.parameters(), lr=learning_rate)
+v_loss_func = nn.MSELoss(reduction='sum')  # ?
+
+# main loop
+for i in range(episode_num):
+    # record trajectories, reset at the beginning of each episode
+    states = []
+    rewards = []
+    actions = []
+    next_states = []
+    state_values = []
+    log_probs = []
+    state = env.reset()  # initialize state, a 1x4 array
+    while True:
+        # interact with the environment
+        action, log_prob = choose_action(state)
+        next_state, reward, done, _ = env.step(action.item())
+        # start recording
+        states.append(state)
+        rewards.append(reward)
+        actions.append(action)
+        next_states.append(next_state)
+        state_values.append(v_net(torch.as_tensor(state,
+                                                  dtype=torch.float32)).detach().item())  # cut down back propagation, don't need gradient calcu later
+        log_probs.append(log_prob)
+        state = next_state
+        if done:
+            break
+    # after a single episode is finished
+    """
+    optimize policy network
+    loss = - log_probabilities * advantage func
+    """
+    states = torch.tensor(states, dtype=torch.float32)
+    actions = torch.tensor(actions, dtype=torch.float32)
+
+    policy_optimizer.zero_grad()
+    policy_loss = comput_policy_loss(log_probs, rewards, state_values)
+    policy_loss.backward()
+    policy_optimizer.step()
+
+    """
+    optimize state value network
+    loss = MSE(reward2go v values, estimated v values)
+    """
+    v_optimizer.zero_grad()
+    rewards2go = compute_advantage(rewards, state_values)[1]
+    estimate_v = v_net(states).squeeze()
+    v_loss = v_loss_func(rewards2go, estimate_v)
+    v_loss.backward(retain_graph=True)  # ?
+    v_optimizer.step()
+
+    # verification
+    result, distance = verify()
+
+    # output info
+    print("episode:{}  reward:{}  distance:{}".format(i, result, distance))
+    wandb.log({"epoch": i, "reward": result, "distance":distance})
